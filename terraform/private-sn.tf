e085ec025a63e2b4e9c00bc0ecd64581bfd880ef
papp <papp.csenge0@gmail.com>
2022-10-18 21:24:06 +0200

    feat: add private subnet

---
+++ terraform/private-sn.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "${var.prefix}-private-subnet"
+  }
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_internet_gateway" "internet-gateway" {
+resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
