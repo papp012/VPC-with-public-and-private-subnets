aca9500f705da6f6c035d9ed44694d32c16eaec5
papp <papp.csenge0@gmail.com>
2022-10-18 21:49:21 +0200

    feat: create route table for the private subnet

--- terraform/private-sn.tf
+++ terraform/private-sn.tf
@@ -7,3 +7,22 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.prefix}-private-rt"
+  }
+}
+
+
+resource "aws_route_table_association" "private_rt_association" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
